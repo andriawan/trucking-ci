c842c6436819e7958ce92f623295dbf15852d10b
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2017-09-29 08:44:42 +0700

    inisialisasi Dashboard modul

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends MY_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->ionAuthCheck();
+	}
+
+	public function index()
+	{
+		echo "logged in";
+		//$this->load->view('dashboard');
+	}
+
+}
+
+/* End of file Dashboard.php */
+/* Location: ./application/controllers/Dashboard.php */
