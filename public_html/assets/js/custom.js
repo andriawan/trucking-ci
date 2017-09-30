6028fe19363afed81c02e00b87bbb0d575471f3b
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2017-09-30 13:16:32 +0700

    penambahan jquery ui library

---
+++ public_html/assets/js/custom.js
@@ -0,0 +1,12 @@
+
+// datepicker jquery ui
+$(".form-control.date").datepicker({
+
+	altField: "#actualDate",
+	dateFormat: "dd-mm-yy",
+	changeMonth: true,
+	changeYear: true,
+	autoSize: true,
+	yearRange: "-25:+0"
+
+});
