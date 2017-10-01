ca7cf1ecc522152b2e28f6dc73fa06c4924d87c9
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2017-10-01 13:35:41 +0700

    penambahan global var javascript pada footer yang menyimpal url ajax call

--- application/views/components/common/footer.php
+++ application/views/components/common/footer.php
@@ -2,4 +2,12 @@
 <!-- js here -->
 <?php $this->load->view('components/vendor/js.php'); ?>
 <?php $this->load->view('components/custom/js.php'); ?>
+<!-- global js var -->
+
+<script type="text/javascript">
+
+	var categoryAllUrl = "<?php echo base_url('dashboard/getCategory') ?>";
+	var categoryByUrl = "<?php echo base_url('dashboard/getby') ?>";
+	
+</script>
 </html>
