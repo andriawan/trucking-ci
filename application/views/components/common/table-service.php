dbefcb139bccc55747d06092e7a8844882622557
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2017-10-05 16:45:04 +0700

    styling button pada view table service lists

--- application/views/components/common/table-service.php
+++ application/views/components/common/table-service.php
@@ -14,10 +14,19 @@
 			<td><?php echo $row['category']; ?></td>
 			<td><?php echo $row['price']; ?></td>
 		<td>
-              
-                <a href="<?php echo site_url('dashboard/serviceedit/'.$row['id_service']); ?>">Edit</a> | 
-                <a href="<?php echo site_url('dashboard/servicedelet/'.$row['id_service']); ?>" onClick="return confirm('Are you sure you want to delete?')">Delete</a>
-            </td>
+			<a href="<?php echo site_url('dashboard/serviceedit/'.$row['id_service']); ?>">         			
+        			<button  class="btn btn-success edit">
+        				<i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+        			</button>
+        	</a>
+
+        	<a href="<?php echo site_url('dashboard/servicedelet/'.$row['id_service']); ?>" onClick="return confirm('Are you sure you want to delete?')">                  
+                    <button  class="btn btn-danger hapus">
+                    	<i class="fa fa-trash-o" aria-hidden="true"></i> Delete
+                    </button>
+            </a>
+
+         </td>
 		</tr>
 		<?php $counter++ ?>
 	<?php endforeach ?>
